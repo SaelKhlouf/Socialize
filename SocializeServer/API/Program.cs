239c1eb6f95cf4ef6b90b01bacb9b3444b920e5b
SaelKhlouf <sael.khlouf@gmail.com>
2021-05-20 23:53:51 +0300

    (Server) Applying migration when running the app + Seeding Data

--- SocializeServer/API/Program.cs
+++ SocializeServer/API/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,25 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            using IServiceScope scope = host.Services.CreateScope();
+            // 'using' with IServiceScope will dispose the object and instances that were fetched from the IoC container, once this method is finished.
+            var services = scope.ServiceProvider; //IServiceProvider
+
+            try
+            {
+                ApplicationDbContext context = services.GetRequiredService<ApplicationDbContext>(); // Service locator pattern
+                context.Database.Migrate();
+                await Seed.SeedData(context);
+            }
+            catch (Exception ex)
+            {
+                ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
+            }
+            host.Run(); // Run the application
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
