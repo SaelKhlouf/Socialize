feb01dd611a63852d1211646803f282a15ddb0b1
SaelKhlouf <sael.khlouf@gmail.com>
2021-07-27 22:43:33 +0300

    (Server) Adding Identity Package and Doing updates to existing packages

--- SocializeServer/Persistence/Seeds/Seed.cs
+++ SocializeServer/Persistence/Seeds/Seed.cs
@@ -1,12 +1,10 @@
-﻿using Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Domain.Activities;
 
-namespace Persistence
+namespace Persistence.Seeds
 {
     public class Seed
     {
